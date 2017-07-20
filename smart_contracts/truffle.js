f7ac007d56c5e9b24c039c3247745caee4dcbe00
Marcus Hearne <marcus@ocelotinnovations.com>
2017-07-20 12:43:35 -0400

    Beacon commit

--- smart_contracts/truffle.js
+++ smart_contracts/truffle.js
@@ -3,7 +3,9 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*" ,
+      gas: 460000// Match any network id
+
     }
   }
 };
